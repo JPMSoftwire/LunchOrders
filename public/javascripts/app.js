50178fde50064773fe7d1bef30b293e272493916
Olly Levett <oliver.levett@softwire.com>
2015-03-23 16:12:30 +0000

    Re-order CSS requires

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,6 @@
 var $ = require("jquery");
-require("../stylesheets/main.css");
 require("bootstrap/dist/css/bootstrap.css");
+require("../stylesheets/main.css");
 var bootstrap = require("bootstrap/dist/js/bootstrap.js");
 
 console.log("Something");
